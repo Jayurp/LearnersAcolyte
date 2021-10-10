f8639ce36f6da76bfb719d70a5dda1bd369a9142
Jayur <pjayur14@gmail.com>
2021-10-10 20:13:07 +0530

    db

---
+++ app/src/main/java/com/example/learnersacolyte/DbHelper.java
@@ -0,0 +1,51 @@
+package com.example.learnersacolyte;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper
+{
+    public static final String db_name = "MyDatabase";
+    public String table_name = "Registration";
+    public String col_name = "name";
+    public String col_enr = "enr";
+    public String col_sem = "sem";
+    public String col_dept = "dept";
+    public String col_institute = "institute";
+
+    public DbHelper(Context context)
+    {
+        super(context,db_name,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String create_table = "create table "+table_name+" ("+col_name+" string NOT NULL,"+col_enr+" integer NOT NULL,"+col_sem+" integer NOT NULL,"+col_dept+" string NOT NULL,"+col_institute+" string NOT NULL);";
+        db.execSQL(create_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        onCreate(db);
+    }
+
+    public void insertUserDetails(String dname, int denr,int dsem, String ddept, String dinst)
+    {
+        //Get the Data Repository in write mode
+        SQLiteDatabase db = this.getWritableDatabase();
+        //Create a new map of values, where column names are the keys
+        ContentValues cValues = new ContentValues();
+        cValues.put(col_name, dname);
+        cValues.put(col_enr, denr);
+        cValues.put(col_sem, dsem);
+        cValues.put(col_dept, ddept);
+        cValues.put(col_institute, dinst);
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(table_name,null, cValues);
+        db.close();
+    }
+}
--- app/src/main/java/com/example/learnersacolyte/MainActivity.java
+++ app/src/main/java/com/example/learnersacolyte/MainActivity.java
@@ -6,23 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button next;
+    DbHelper myDb = new DbHelper(this);
+    EditText name,enr,sem,dept,inst;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportActionBar().hide();
 
-        btn = findViewById(R.id.button);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        next = findViewById(R.id.button);
+        name = findViewById(R.id.name);
+        enr = findViewById(R.id.enr);
+        sem = findViewById(R.id.sem);
+        dept = findViewById(R.id.dept);
+        inst = findViewById(R.id.inst);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String dname = name.getText().toString();
+                String denr = enr.getText().toString();
+                String dsem = sem.getText().toString();
+                String ddept = dept.getText().toString();
+                String dinst = inst.getText().toString();
+
+                int enr_value = Integer.parseInt(denr);
+                int sem_value = Integer.parseInt(dsem);
+
+                myDb.insertUserDetails(dname,enr_value,sem_value,ddept,dinst);
                 start();
             }
         });
